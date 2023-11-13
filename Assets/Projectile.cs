122a2ff9873c4efa21f7c36eb2983326f676ce6d
ArunJ0 <anttijussi.niku@gmail.com>
2023-11-13 19:42:43 +0200

    Weaponilla on nyt reload toiminto ja muokkasin niin että mitä koodissa on näyttää lucidchartin mukaiselta

--- Assets/Projectile.cs
+++ Assets/Projectile.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {    
-    public float projectile_death_time = 2;
-    public Transform TF;
+    //projectiles death timer to make sure the projectile dies after 3 seconds
+    public float projectile_death_time = 3.0f;
     void Awake()
     {
-        TF = GetComponent<Transform>();
         Destroy(gameObject, projectile_death_time);
     }
- 
+    // when the projectile collides with something this happens
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("biplane_tag"))
@@ -19,7 +18,7 @@ public class Projectile : MonoBehaviour
             Debug.Log("Virhe: ammus osui lentokoneeseen");
         }
         else {
-            Destroy(collision.gameObject);
+            //Destroy(collision.gameObject);
         }
         Destroy(gameObject);
     }
--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -8,19 +8,53 @@ public class Weapon : MonoBehaviour
 {
     public Transform ProjectilesSpawn;
     public GameObject projectilePrefab;
-    public float projectilespeed = 10f;
     public PlayerInputButton ShootButton;
 
-    private float lastShotTime;
-    public float cooldownDuration = 2f;
-    void Update()
+    public Vector3 projectileScale = new Vector3(0.5f,0.5f,0.5f);
+    //Projectile scale
+
+    public int ammo_max = 10;
+    public int ammo_amount = 10;
+    //Ammo count of the weapon
+    public float reload_speed = 2.0f;
+    //Reload speed at which ammo come back after shooting
+    public float projectile_speed = 18.0f;
+    //Projectile speed aka bullet speed
+    public float fire_rate = 2.0f;
+    //Projectile fire rate after which you can shoot again, the lower it is the faster you can shoot
+    public const float projectile_Damage = 0.0f;
+    // damage of the weapons projectiles
+
+    private float lastShotTime = -100.0f;
+    // time of the last shot
+    private float lastReloadTime = 0.0f;
+    // time of the last reload
+
+    void FixedUpdate()
     {
-        if(ShootButton.ButtonState()==true && Time.time - lastShotTime >= cooldownDuration)
+        // cooldown duration has passed since last reload
+        if(ammo_amount<ammo_max && Time.time - lastReloadTime >= reload_speed)
         {
-            var projectile = Instantiate(projectilePrefab, ProjectilesSpawn.position, ProjectilesSpawn.rotation);
-            projectile.GetComponent<Rigidbody>().velocity = ProjectilesSpawn.forward * projectilespeed * 3;
+            lastReloadTime = Time.time;
+            ammo_amount = ammo_amount + 1;
+        }
+        // button is pressed and cooldown duration has passed since last shot
+        if (ShootButton.ButtonState()==true && Time.time - lastShotTime >= fire_rate && ammo_amount >= 1)
+        {
+            ShootProjectile();
+            //sets the time for the shot
             lastShotTime = Time.time;
         }
+        print(Time.time);
     }
-
+    void ShootProjectile()
+    {
+        ammo_amount = ammo_amount - 1;
+        //creates the projectile and sets its direction
+        var projectile = Instantiate(projectilePrefab, ProjectilesSpawn.position, ProjectilesSpawn.rotation);
+        //changes projectile scale
+        projectile.gameObject.transform.localScale = projectileScale;
+        //sets the speed of the projectile
+        projectile.GetComponent<Rigidbody>().velocity = ProjectilesSpawn.forward * projectile_speed;
+    }
 }
