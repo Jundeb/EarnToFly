a8c595ef998a99666bb997c152941652983c54ba
ArunJ0 <anttijussi.niku@gmail.com>
2023-12-04 15:55:34 +0200

    OptionsMenuSetup+Fullscreen+VolumeSystemInit

--- Assets/Scripts/StartingMenu.cs
+++ Assets/Scripts/StartingMenu.cs
@@ -5,12 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class StartingMenu : MonoBehaviour
 {
+    public GameObject OptionsPanel;
+    public GameObject MainMenuPanel;
+    // MAIN MENU
     public void StartGame()
     {
         SceneManager.LoadScene(1);
     }
+    public void OptionsButton()
+    {
+        MainMenuPanel.SetActive(false);
+        OptionsPanel.SetActive(true);
+    }
     public void QuitGame()
     {
         Application.Quit();
+    }
+    // OPTIONS MENU
+    public void ReturnButton()
+    {
+        OptionsPanel.SetActive(false);
+        MainMenuPanel.SetActive(true);
+    }
+    public void ToggleFullscreen()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
     }
 }
