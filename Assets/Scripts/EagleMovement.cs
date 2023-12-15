be3b1d92b979e1db73b44931d26978fcef5e2780
Junnu Kyrö <junnukyro@outlook.com>
2023-12-15 17:55:26 +0200

    Merge branch 'develop' of https://github.com/luris123/EarnToFly into develop

--- Assets/Scripts/EagleMovement.cs
+++ Assets/Scripts/EagleMovement.cs
@@ -12,34 +12,24 @@ public class EagleMovement : MonoBehaviour
     void Start()
     {
         eagle = GetComponent<Transform>();
-        wing1 = GameObject.FindGameObjectWithTag("EagleWing1").GetComponent<Transform>();
-        wing2 = GameObject.FindGameObjectWithTag("EagleWing2").GetComponent<Transform>();
+        wing1 = transform.Find("Wing1");
+        wing2 = transform.Find("Wing2");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (eagle != null)
-        {
-            if (wing1 != null)
-            {
-                wing1.RotateAround(wing1.position, wing1.right, Mathf.Sin(Time.time * 4) * 2f);
-            }
-            else
-            {
-                Debug.LogError("Wing1 is null");
-            }
 
-            if (wing2 != null)
-            {
-                wing2.RotateAround(wing2.position, wing2.right, Mathf.Sin(Time.time * 4) * -2f);
-            }
-            else
-            {
-                Debug.LogError("Wing2 is null");
-            }
+        if (wing1 != null)
+        {
+            wing1.RotateAround(wing1.position, wing1.right, Mathf.Sin(Time.time * 4) * 2);
+        }
+
+        if (wing2 != null)
+        {
+            wing2.RotateAround(wing2.position, wing2.right, Mathf.Sin(Time.time * 4) * -2);
         }
     }
+
 
     private void FixedUpdate()
     {
